68c6d30257a052d87e475c85273f355a9a429f86
Utkarsh Chaudhary <38369815+uc129@users.noreply.github.com>
2024-12-27 12:41:01 +0530

    ''

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     const body = await req.json();
     console.log(body);
     return NextResponse.json({ message: 'POST request to the signup route', });
